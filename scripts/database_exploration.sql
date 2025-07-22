5ebcceb37bdcd7e6fef29459c602014f3e2e8c78
Emre Kandir <75427107+kyem404@users.noreply.github.com>
2025-07-22 10:02:32 +0300

    Delete scripts/database_exploration.sql

--- scripts/database_exploration.sql
+++
@@ -1,6 +0,0 @@
--- Explore All Objects in the Database
-SELECT * FROM INFORMATION_SCHEMA.TABLES
-
--- Explore All Columns in the Database
-SELECT * FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'dim_customers'
