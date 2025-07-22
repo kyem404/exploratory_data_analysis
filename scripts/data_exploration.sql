7391fd02bb2c121d801778d75912cc1c77573c68
Emre Kandir <75427107+kyem404@users.noreply.github.com>
2025-07-22 22:53:08 +0300

    Update data_exploration.sql

--- scripts/data_exploration.sql
+++ scripts/data_exploration.sql
@@ -187,3 +187,77 @@ ORDER BY total_sold_items DESC;
 -- Top N performers | Bottom N Performers
 -------------------------------------------------
 
+-- Top 5 products that generate the highest revenue
+SELECT TOP 5
+p.product_name,
+SUM(f.sales_amount) total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue DESC;
+
+-- for higher complexity of data
+SELECT * 
+FROM (
+	SELECT
+	p.product_name,
+	SUM(f.sales_amount) total_revenue,
+	ROW_NUMBER() OVER (ORDER BY SUM(f.sales_amount) DESC) AS rank_products
+	FROM gold.fact_sales f
+	LEFT JOIN gold.dim_products p
+	ON p.product_key = f.product_key
+	GROUP BY p.product_name
+)t 
+WHERE rank_products <= 5
+
+
+-- Top 5 product that perform the worst
+SELECT TOP 5
+p.product_name,
+SUM(f.sales_amount) total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue;
+
+-- Top 5 subcategories that generate the highest revenue
+SELECT TOP 5
+p.subcategory,
+SUM(f.sales_amount) total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+GROUP BY p.subcategory
+
+
+-- Top 10 customers who have generated the highest revenue + 3 customers with the fewest orders placed
+SELECT TOP 10
+c.customer_key,
+c.first_name,
+c.last_name,
+SUM(f.sales_amount) total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON c.customer_key = f.customer_key
+GROUP BY 
+c.customer_key, 
+c.first_name, 
+c.last_name
+ORDER BY total_revenue DESC;
+
+
+SELECT TOP 3
+c.customer_key,
+c.first_name,
+c.last_name,
+COUNT(DISTINCT order_number) total_orders
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON c.customer_key = f.customer_key
+GROUP BY 
+c.customer_key, 
+c.first_name, 
+c.last_name
+ORDER BY total_orders;
